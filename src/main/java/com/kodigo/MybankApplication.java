1fbfbe2a6b3a9cd0a1af7d965007637e7dd409a8
David Onofre <88davido@gmail.com>
2023-02-25 00:44:53 -0500

    prueba 2 source tree

--- src/main/java/com/kodigo/MybankApplication.java
+++ src/main/java/com/kodigo/MybankApplication.java
@@ -8,7 +8,7 @@ public class MybankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MybankApplication.class, args);
-		System.out.println("kodigo2");
+		System.out.println("kodigo3");
 	}
 
 }
