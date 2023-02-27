475993a275ce7f2d8077d13e3f7ee1f2384dc1bc
David Onofre <88davido@gmail.com>
2023-02-26 22:29:27 -0500

    Pruebas Unitarias

--- src/test/java/com/kodigo/PersonaServiceMockTest.java
+++ src/test/java/com/kodigo/PersonaServiceMockTest.java
@@ -3,23 +3,21 @@ package com.kodigo;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.kodigo.model.Persona;
-import com.kodigo.repo.IPersonaRepo;
+import com.kodigo.service.IPersonaService;
 
 @SpringBootTest
-public class PersonaRepoTest {
+public class PersonaServiceMockTest {
 
 	@Autowired
-	private IPersonaRepo repo;
+	private IPersonaService service;
 
 	@Test
-	void grabarPersona() {
+	void grabarPersona() throws Exception {
 		Persona persona = new Persona();
 		persona.setNombre("Jonatan Onofre");
 		persona.setGenero("M");
@@ -27,31 +25,29 @@ public class PersonaRepoTest {
 		persona.setIdentificacion("1719555987");
 		persona.setDireccion("Quicentro");
 		persona.setTelefono("0955567845");
-		Persona retorno = repo.save(persona);
+		Persona retorno = service.registrar(persona);
 		assertTrue(retorno.getIdPersona() != 0);
 	}
 
 	@Test
-	void obtenerPersona() {
+	void obtenerPersona() throws Exception {
 		Integer idPesona = 1;
-		Optional<Persona> optional = repo.findById(idPesona);
-		Persona retorno = optional.get();
+		Persona retorno = service.listarPorId(idPesona);
 		assertTrue(retorno.getIdPersona() != null);
 	}
 
 	@Test
-	void modificarPersona() {
+	void modificarPersona() throws Exception {
 		Integer idPesona = 2;
-		Optional<Persona> optional = repo.findById(idPesona);
-		Persona retorno = optional.get();
+		Persona retorno = service.listarPorId(idPesona);
 		retorno.setNombre("Darío Onofre");
-		repo.save(retorno);
+		service.registrar(retorno);
 		assertTrue(retorno.getNombre() == "Darío Onofre");
 	}
 
 	@Test
 	void eliminarPersona() {
 		Integer idPersona = 999;
-		assertThatThrownBy(() -> repo.deleteById(idPersona));
+		assertThatThrownBy(() -> service.eliminar(idPersona));
 	}
 }
